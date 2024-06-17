5e0009542974f2536b0fbe71a32a699506fa6815
Simran <itssimran903@gmail.com>
2024-06-17 22:11:09 +0530

    Created Navbar

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar"
 function App() {
 
   return (
-    <div className="min-h-screen text-white bg-sky-950">
+    <div className="min-h-screen bg-sky-950">
       <Navbar />
     </div>
   )
