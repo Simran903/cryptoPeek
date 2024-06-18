6a182e6d74e234a7c8eeaaacab8bcd082853baa0
Simran <itssimran903@gmail.com>
2024-06-18 23:25:20 +0530

    created navbar

--- my-project/src/components/Navbar.tsx
+++ my-project/src/components/Navbar.tsx
@@ -2,8 +2,14 @@ import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuList,
-  NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 const Navbar = () => {
   return (
@@ -11,17 +17,24 @@ const Navbar = () => {
       <img src="./logo.png" alt="" width="150px" />
       <NavigationMenu className="mx-auto">
         <NavigationMenuList className="space-x-10">
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>HOME</NavigationMenuTrigger>
+          <NavigationMenuItem className="inline-flex cursor-pointer h-9 w-max items-center justify-center rounded-md bg-lime-400 px-4 py-2 text-sm font-medium text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800">HOME
           </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>FEATURES</NavigationMenuTrigger>
+          <NavigationMenuItem className="inline-flex cursor-pointer h-9 w-max items-center justify-center rounded-md bg-lime-400 px-4 py-2 text-sm font-medium text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800">FEATURES
           </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>PRICING</NavigationMenuTrigger>
+          <NavigationMenuItem className="inline-flex cursor-pointer h-9 w-max items-center justify-center rounded-md bg-lime-400 px-4 py-2 text-sm font-medium text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800">PRICING
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
+      <div className="pr-12">
+        <DropdownMenu>
+          <DropdownMenuTrigger className=" inline-flex h-9 w-max items-center justify-center rounded-md bg-lime-400 px-4 py-2 text-sm font-medium text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800">Currency</DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>USD</DropdownMenuItem>
+            <DropdownMenuItem>EUR</DropdownMenuItem>
+            <DropdownMenuItem>INR</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 };
